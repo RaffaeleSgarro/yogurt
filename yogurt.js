018e8d353aa5bd2973950ca934d15b34615c6512
RaffaeleSgarro <raffaeleSgarro@gmail.com>
2012-07-15 16:32:52 +0200

    Fixed bug when upside down image is cached

--- yogurt.js
+++ yogurt.js
@@ -2,24 +2,34 @@
 
 // On dom loaded
 $(function() {
-	var start_animation = function(element) {
+	var loop = function(element) {
 		element.delay(5000).animate({top: 0}, 3000, "easeInBounce")
 		.promise().pipe(function() {
 			element.delay(2000).animate({top: -element.height()});
 		})
-	    .promise().pipe(function() {
-			start_animation(element);
+		.promise().pipe(function() {
+			loop(element);
 		});
 	}
 	
 	var bunny = $("#upside_down");
+	var img = bunny.find("img");
 	
-	// Wait for image to be loaded.
-	$("img", bunny).bind("load", function(evt) {
-		var height = $(evt.target).height();
-		bunny.css("top", - bunny.height() + "px");
-		start_animation(bunny);
-	});
+	var start_animation = function (image) {
+		// Hide the rabbit
+		bunny.css("top", ( -image.height() ) + "px");
+		// Start the loop
+		loop(bunny);
+	}
+	
+	// Wait for image to be loaded, watch for cached images
+	if (img[0].complete) {
+		start_animation(img);
+	} else {
+		bunny.find("img").bind("load", function(evt) {
+			start_animation($(evt.target));
+		});
+	}
 	
 });
 
