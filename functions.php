b2aa0bd06084fd6ad8e9dfade9251e4da9fc2abf
RaffaeleSgarro <raffaeleSgarro@gmail.com>
2012-07-15 12:42:16 +0200

    Animated bunny

--- functions.php
+++ functions.php
@@ -2,6 +2,17 @@
 
 add_theme_support('automatic-feed-links');
 add_action( 'init', 'register_my_menus' );
+add_action('wp_enqueue_scripts', 'register_my_scripts');
+
+function register_my_scripts() {
+	wp_enqueue_script(
+		'yogurt',
+		get_template_directory_uri() . "/yogurt.js",
+		array('jquery-effects-core')
+	);
+}    
+ 
+
 
 function register_my_menus() {
   register_nav_menus(array(
